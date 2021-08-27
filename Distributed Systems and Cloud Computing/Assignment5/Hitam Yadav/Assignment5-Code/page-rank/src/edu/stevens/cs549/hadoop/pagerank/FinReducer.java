a0b3fc279af11c70ed1b0165554e2d9fcff89178
HitamYadav <49002850+HitamYadav@users.noreply.github.com>
2021-08-27 15:09:00 -0400

    Add files via upload

---
+++ Distributed Systems and Cloud Computing/Assignment5/Hitam Yadav/Assignment5-Code/page-rank/src/edu/stevens/cs549/hadoop/pagerank/FinReducer.java
@@ -0,0 +1,25 @@
+package edu.stevens.cs549.hadoop.pagerank;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class FinReducer extends Reducer<DoubleWritable, Text, Text, Text> {
+
+	public void reduce(DoubleWritable key, Iterable<Text> values, Context context) throws IOException,
+			InterruptedException {
+		/* 
+		 * TODO: For each value, emit: key:value, value:-rank
+		 */
+		Iterator<Text> iterator = values.iterator();
+		String node;
+		while(iterator.hasNext()) {
+			node = iterator.next().toString();
+			
+			context.write(new Text(node), new Text(String.valueOf(0 - key.get())));
+		}
+	}
+}
+
